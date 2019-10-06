1d602e65a0713040c3bb4da2db3dbbbc565919e1
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2019-10-06 15:40:12 -0600

    Lecture 10: Label Showing to the Player (3.1.1) Commit

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,9 +5,7 @@ extends Control
 func _ready():
 	var StoryWords = ["Bob", "fast", "tired" , "bed" ]
 	var StoryScript = "In the beginning, there was %s and he was %s. In the end, he felt %s, and thus went to %s."
-	print(StoryScript % StoryWords)
-	
-	StoryWords = ["Ted", "slow", "hyper" , "the game" ]
-	print(StoryScript % StoryWords)
-	
+
+	get_node("DisplayText").text = StoryScript % StoryWords
+	#Shortcut is $DisplayText.text = "Text 1" and will be used from now on
 
