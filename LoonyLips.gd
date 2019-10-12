a12388333e8f55b18a685106cca0effa58772930
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2019-10-12 15:53:53 -0600

    Lecture 10: Appending Arrays and “If” conditions (3.1.1)  Commit1 (WORKING)

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,25 +1,50 @@
 extends Control
 
+var player_words = []
+var StoryIntro = "Welcome to Loony lips, a fun party game to tell weird stories!\n\n"
+var StoryScript = "In the beginning, there was %s and he was %s. In the end, he felt %s, and thus went to %s."
+var StoryWordType = ["a name", "an adverb", "an adjective" , "a noun" ]
 
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
-	var StoryWords = ["Bob", "fast", "tired" , "bed" ]
-	var StoryScript = "In the beginning, there was %s and he was %s. In the end, he felt %s, and thus went to %s."
-	$VBoxContainer/DisplayText.text = StoryScript % StoryWords
+	if player_words.size() == 0:
+		DisplayText.text = StoryIntro + "May I have " + StoryWordType[player_words.size()] + " please ?"
+	else:
+		check_player_words_length()
+	
 
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+	add_to_player_words()
 
 
 func _on_ProceedButton_pressed():
-	var words = $VBoxContainer/HBoxContainer/PlayerText.text
-	update_DisplayText(words)
+	add_to_player_words()
 	
 
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	PlayerText.clear();
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == StoryWordType.size()
 
 
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
 
+
+func tell_story():
+	DisplayText.text  = StoryScript % player_words
+
+
+func prompt_player():
+	DisplayText.text = "May I have " + StoryWordType[player_words.size()] + " please ?"
+	
