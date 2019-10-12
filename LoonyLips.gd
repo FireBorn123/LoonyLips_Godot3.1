ff9e9fd7faf0c525399822d399920fad14435e2f
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2019-10-12 13:31:46 -0600

    Lecture 11: LineEdit – Getting Text from the Player (3.1.1) Commit

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,7 +5,14 @@ extends Control
 func _ready():
 	var StoryWords = ["Bob", "fast", "tired" , "bed" ]
 	var StoryScript = "In the beginning, there was %s and he was %s. In the end, he felt %s, and thus went to %s."
+	$VBoxContainer/DisplayText.text = StoryScript % StoryWords
 
-	get_node("DisplayText").text = StoryScript % StoryWords
-	#Shortcut is $DisplayText.text = "Text 1" and will be used from now on
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
 
