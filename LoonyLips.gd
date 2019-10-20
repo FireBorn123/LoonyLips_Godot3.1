87c327372f48b5be2d93fd43588a9f97a1af974d
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2019-10-20 17:07:09 -0600

    Lecture 12: Storybook Option 2: JSON (3.1.1)  Commit

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -2,30 +2,7 @@ extends Control
 
 var player_words = []
 var StoryIntro = "Welcome to Loony lips, a fun party game to tell weird stories!\n\n"
-var Template = [
-		{
-		"StoryWordType":["a name", "an adverb", "an adjective" , "a noun" ],
-		"StoryScript" : "In the beginning, there was %s and he was %s. In the end, he felt %s, and thus went to %s."
-		}, 
-		
-		{"StoryWordType" : ["a noun", "an adjective", "an noun" , "a verb" ],
-		"StoryScript" : "In the end the world was on %s. It was a %s day, and there was much %s in prepration. However, humanity was determined to %s. In the end, he felt %s, and thus went to %s."
-		}, 
-		
-		{
-		"StoryWordType" : ["name", "number",  "fast food", "number", "openable object"],
-		"StoryScript" : "Once upon a time %s bought %s %s's even though only %s cound fit in the %s."
-		},
-		
-		{
-		"StoryWordType" : ["name", "object",  "adjective", "adverb"],
-		"StoryScript" : "In a land never seen %s found a %s, and used it for his own %s purposes. From there time progressed %s"
-		},
-		
-		{
-		"StoryWordType" : ["object", "noun",  "place", "adjective", "adjective", "adjective", "noun"],
-		"StoryScript" : "In a galexy far far away a tiny %s tried to find %s way to the %s. It was a %s and %s jorney that was %s with %s."
-		}]
+
 
 var CurrentStory 
 
@@ -40,9 +17,22 @@ func _ready():
 	PlayerText.grab_focus()
 	
 
+
 func set_current_story():
+	var stories = read_JSON_File("StoryBook.json")
 	randomize()
-	CurrentStory = Template[randi() % Template.size()]
+	CurrentStory = stories[randi() % stories.size()]
+#	CurrentStory = Template[randi() % Template.size()]
+
+
+func read_JSON_File(StoryFileName):
+	var file = File.new()
+	file.open(StoryFileName, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
+
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
